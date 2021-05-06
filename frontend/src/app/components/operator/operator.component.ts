9e043eac73a18173ee82b657c3050d0bce4647df
Giancarlo Buenaflor <e51837398@student.tuwien.ac.at>
2021-05-06 21:38:22 +0200

    [#33] Improve styling of the sidemenu items

--- frontend/src/app/components/operator/operator.component.ts
+++ frontend/src/app/components/operator/operator.component.ts
@@ -8,10 +8,7 @@ import {Router} from '@angular/router';
 })
 export class OperatorComponent implements OnInit {
 
-  constructor(private router: Router) { }
-
   ngOnInit(): void {
-    this.router.navigate(['operator/home']);
   }
 
 }
