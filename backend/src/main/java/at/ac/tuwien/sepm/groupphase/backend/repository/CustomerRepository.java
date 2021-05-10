00ff82e555155f47c00e40a985c9b0e1c6c9a612
Simon Roth <e11907576@student.tuwien.ac.at>
2021-05-10 14:43:28 +0200

    [#22] Added service for address

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/CustomerRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/CustomerRepository.java
@@ -4,10 +4,12 @@ import at.ac.tuwien.sepm.groupphase.backend.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    @Query(value = "INSERT INTO customer(id,name,login_name,password,email,address,phone_number) VALUES :id,:name,:login_name,:password,:email,:address_id,:phone_number", nativeQuery = true)
+    @Query(value = "INSERT INTO customer(id,name,login_name,password,email,address_id,phone_number) VALUES :id,:name,:login_name,:password,:email,:address_id,:phone_number", nativeQuery = true)
     Customer registerNewCustomer(@Param("id") Long id, @Param("name") String name, @Param("login_name") String loginName, @Param("password") String password, @Param("email") String email, @Param("address_id") Long address,
                                  @Param("phone_number") String phoneNumber);
 }
---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/AddressServiceImpl.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.sepm.groupphase.backend.service.impl;
+
+import at.ac.tuwien.sepm.groupphase.backend.entity.Address;
+import at.ac.tuwien.sepm.groupphase.backend.service.AddressService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+    @Override
+    public Address addNewAddress(Address address) {
+        return null;
+    }
+}
