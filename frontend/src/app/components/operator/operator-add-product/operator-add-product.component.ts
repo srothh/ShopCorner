d1b17af337501c35b0e0a54f0d4eade42d245d3c
Hafiz Khan <hafiz_10@hotmail.com>
2021-05-13 14:57:52 +0200

    [#27] frontend: added template with form and inputs and image selection

--- frontend/src/app/components/operator/operator-add-product/operator-add-product.component.ts
+++ frontend/src/app/components/operator/operator-add-product/operator-add-product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-operator-add-product',
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OperatorAddProductComponent implements OnInit {
 
+  @ViewChild('fileInput')
+  fileInput: ElementRef;
+  fileToUpload: File;
+  fileSource: string | ArrayBuffer;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSelectFile(event){
+    this.fileToUpload = event.target.files.item(0);
+    const reader = new FileReader();
+    reader.readAsDataURL(this.fileToUpload);
+    reader.onload = ((loadEvent) => {
+      this.fileSource = loadEvent.target.result;
+    });
+
+  }
+  clearImage(){
+    this.fileToUpload = undefined;
+    this.fileSource = undefined;
+    this.fileInput.nativeElement.value = '';
   }
 
 }
