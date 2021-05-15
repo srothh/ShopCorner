463af69d3689609ac82ea805971bc0b791e9eedd
Christian Ammann <e11713963@student.tuwien.ac.at>
2021-05-16 00:32:08 +0200

    [#25] added the pdf generator to the invoice.ts file

--- frontend/src/app/components/operator/operator-invoice/operator-invoice.component.ts
+++ frontend/src/app/components/operator/operator-invoice/operator-invoice.component.ts
@@ -5,7 +5,7 @@ import {Product} from '../../../dtos/product';
 import {Invoice} from '../../../dtos/invoice';
 import {FormBuilder, FormGroup, FormArray, Validators, FormControl} from '@angular/forms';
 import {InvoiceService} from '../../../services/invoice.service';
-import {toString} from '@ng-bootstrap/ng-bootstrap/util/util';
+
 import {formatDate} from '@angular/common';
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
@@ -21,6 +21,9 @@ export class OperatorInvoiceComponent implements OnInit {
   error = false;
   errorMessage = '';
   invoiceDto: Invoice;
+
+  mapItem = {};
+  map = [];
 
   constructor(private invoiceService: InvoiceService, private formBuilder: FormBuilder) {
   }
@@ -54,7 +57,9 @@ export class OperatorInvoiceComponent implements OnInit {
       return;
     }
     this.creatInvoiceDto();
-    this.addInvoice();
+    this.generatePdf();
+    // this.generatePdf();
+    // this.addInvoice();
   }
 
   addInvoice() {
@@ -78,10 +83,117 @@ export class OperatorInvoiceComponent implements OnInit {
         this.invoiceDto.invoiceItems.push(product);
       }
       amount += item.value.price * item.value.quantity;
+      this.mapItem['product'] = product;
+      this.mapItem['quantity'] = item.value.quantity;
+      this.map.push(this.mapItem);
     }
     this.invoiceDto.amount = amount;
     this.invoiceDto.date = formatDate(new Date(), 'dd.MM.yyyy HH:mm:ss', 'en');
   }
+
+   generatePdf() {
+     const docDefinition = {
+       content: [
+         {
+           text: 'ELECTRONIC SHOP',
+           fontSize: 16,
+           alignment: 'center',
+           color: '#047886'
+         },
+         {
+           text: 'INVOICE',
+           fontSize: 20,
+           bold: true,
+           alignment: 'center',
+           decoration: 'underline',
+           color: 'skyblue'
+         },
+         {
+           text: 'Customer Details',
+           style: 'sectionHeader'
+         },
+         {
+           columns: [
+             [
+               {
+                 text: 'this.invoice.customerName',
+                 bold: true
+               },
+               { text: 'this.invoice.address' },
+               { text: 'this.invoice.email' },
+               { text: 'this.invoice.contactNo' }
+             ],
+             [
+               {
+                 text: `Date: ${new Date().toLocaleString()}`,
+                 alignment: 'right'
+               },
+               {
+                 text: `Bill No : ${((Math.random() * 1000).toFixed(0))}`,
+                 alignment: 'right'
+               }
+             ]
+           ]
+         },
+         {
+           text: 'Order Details',
+           style: 'sectionHeader'
+         },
+         {
+           table: {
+             headerRows: 1,
+             widths: ['*', 'auto', 'auto', 'auto'],
+             body: [
+               ['Product', 'Price', 'Quantity', 'Amount'],
+
+             ]
+           }
+         },
+         {
+           text: 'Additional Details',
+           style: 'sectionHeader'
+         },
+         {
+           text: 'this.invoice.additionalDetails',
+           margin: [0, 0 , 0, 15]
+         },
+         {
+           columns: [
+             [{ qr: `www.facebook.com`, fit: '50' }],
+             [{ text: 'Signature', alignment: 'right', italics: true}],
+           ]
+         },
+         {
+           text: 'Terms and Conditions',
+           style: 'sectionHeader'
+         },
+         {
+           ul: [
+             'Order can be return in max 10 days.',
+             'Warrenty of the product will be subject to the manufacturer terms and conditions.',
+             'This is system generated invoice.',
+           ],
+         }
+       ],
+       styles: {
+         sectionHeader: {
+           bold: true,
+           decoration: 'underline',
+           fontSize: 14,
+           margin: [0, 15, 0, 15]
+         }
+       }
+     };
+
+
+     for (const item of this.map) {
+       docDefinition['content'][5].table.body.push([item.product.name, item.product.price,
+         item.quantity, item.product.price * item.quantity]);
+     }
+     pdfMake.createPdf(docDefinition).open();
+   }
+
+
 
   onReset() {
     this.submitted = false;
@@ -90,14 +202,13 @@ export class OperatorInvoiceComponent implements OnInit {
     this.addProductOnClick();
   }
 
-  deleteProduct(id: number) {
+  deleteProductFromInvoice(id: number) {
     if (this.t.length > 1) {
       this.t.removeAt(id);
     } else {
       this.errorHandling('Es können nicht alle Elemente einer Rechnung gelöscht werden');
     }
   }
-
 
   /**
    * Error flag will be deactivated, which clears the error message
