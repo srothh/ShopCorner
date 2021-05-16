1ad46fb5dbbad1855d284834521d44136164d83a
Simon Roth <e11907576@student.tuwien.ac.at>
2021-05-16 21:49:56 +0200

    [#22] improved frontend error handling

--- frontend/src/app/interceptors/http-error-interceptor.ts
+++ frontend/src/app/interceptors/http-error-interceptor.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
 import {catchError} from 'rxjs/operators';
 
@@ -9,13 +9,19 @@ export class HttpErrorInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error) => {
-        console.log(error.error);
-        let message = error.error.replace(/[a-zA-Z]*\.[a-zA-Z]*\s/, ' ');
-        message = message.replace(/{Validation errors=\[/,'');
-        console.log(message);
-        message = message.slice(0, -2);
-        return throwError(message
-        );
+        if (error instanceof HttpErrorResponse) {
+          if(error.status === 0){
+            return throwError('backend unreachable');
+          }else{
+            let message = error.error.replace(/[a-zA-Z]*\.[a-zA-Z]*\s/, ' ');
+            message = message.replace(/{Validation errors=\[/, '');
+            message = message.slice(0, -2);
+            return throwError(message
+            );
+          }
+
+        }
+
       })
     );
   }
