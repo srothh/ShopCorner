33ab7799666af236eed7167a8161a044adfb06a8
Michael <e11705362@tuwien.student.ac.at>
2021-05-20 16:12:42 +0200

    [#21] changed error handling in frontend

--- frontend/src/app/components/operator/operator-accounts/operator-account.component.ts
+++ frontend/src/app/components/operator/operator-accounts/operator-account.component.ts
@@ -64,22 +64,9 @@ export class OperatorAccountComponent implements OnInit {
         this.collectionSize=this.admins.length;
       },
       error => {
-        this.defaultServiceErrorHandling(error);
+        this.error = true;
+        this.errorMessage = error.error;
       }
     );
-  }
-
-  private defaultServiceErrorHandling(error: any) {
-    console.log(error);
-    this.error = true;
-    if (error.status === 0) {
-      // If status is 0, the backend is probably down
-      this.errorMessage = 'The backend seems not to be reachable';
-    } else if (error.error.message === 'No message available') {
-      // If no detailed error message is provided, fall back to the simple error name
-      this.errorMessage = error.error.error;
-    } else {
-      this.errorMessage = error.error.message;
-    }
   }
 }
