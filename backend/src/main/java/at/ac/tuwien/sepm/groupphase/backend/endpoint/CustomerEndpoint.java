d066a73c34c11d72a39c5197c71a8dbf4e54914c
Giancarlo Buenaflor <e51837398@student.tuwien.ac.at>
2021-05-24 22:26:53 +0200

    [#23] Add security tests using the customer entity for simplicity

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/CustomerEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/CustomerEndpoint.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,7 +60,7 @@ public class CustomerEndpoint {
      * @return A list of all the retrieved customers
      */
     //TODO Change to Secured(ROLE_ADMIN)
-    @PermitAll
+    @Secured({"ROLE_ADMIN", "ROLE_EMPLOYEE"})
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "retrieve all customers")
