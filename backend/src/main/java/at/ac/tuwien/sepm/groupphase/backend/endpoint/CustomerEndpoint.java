798cef979c29866e4236d3e60d46c6ff3564ddb2
Giancarlo Buenaflor <e51837398@student.tuwien.ac.at>
2021-05-25 23:06:48 +0200

    [#23] Fix checkstyle violation

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/CustomerEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/CustomerEndpoint.java
@@ -11,7 +11,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.annotation.security.PermitAll;
 import javax.validation.Valid;
