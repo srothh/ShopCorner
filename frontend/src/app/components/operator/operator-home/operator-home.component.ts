8520173e651f398681a2ae94fee6fcab045b2175
Anja Kurzmann <e11905151@student.tuwien.ac.at>
2021-05-25 23:00:07 +0200

    [#19] implemented rough version of operator profile page in frontend

--- frontend/src/app/components/operator/operator-home/operator-home.component.ts
+++ frontend/src/app/components/operator/operator-home/operator-home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Operator} from '../../../dtos/operator';
+import {Permissions} from '../../../dtos/permissions.enum';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-operator-home',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OperatorHomeComponent implements OnInit {
 
-  constructor() { }
+  // TODO replace with currently logged in operator
+  operator = new Operator(1, 'test', 'test', 'password', 'test@mail.com', Permissions.employee);
+
+
+  submitted = false;
+  error = false;
+  errorMessage = '';
+
+  constructor(private router: Router) {
+  }
 
   ngOnInit(): void {
+    //TODO fetch currently logged in operator from backend
   }
+
+  goToEdit() {
+    this.router.navigate(['/operator/account/edit']);
+  };
 
 }
