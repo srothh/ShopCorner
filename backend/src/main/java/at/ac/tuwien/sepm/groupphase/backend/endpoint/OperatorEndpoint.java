e87bc37420cec4dd935305939b85501878c56154
Michael <e11705362@tuwien.student.ac.at>
2021-05-26 11:55:33 +0200

    [#17] removed unused import in OperatorEndpoint

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/OperatorEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/OperatorEndpoint.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import javax.annotation.security.PermitAll;
 import javax.validation.Valid;
