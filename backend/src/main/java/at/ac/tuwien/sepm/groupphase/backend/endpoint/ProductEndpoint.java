8c0918498e2c730f7f003ff598e99fd2e088c8d2
Giancarlo Buenaflor <e51837398@student.tuwien.ac.at>
2021-06-03 20:42:41 +0200

    [#83] Change permission from ROLE_ADMIN to ALL on GET product

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/ProductEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/ProductEndpoint.java
@@ -132,7 +132,7 @@ public class ProductEndpoint {
      * @param productId the id to search in the database and retrieve the associated product entity
      * @return the product entity with the associated Id
      */
-    @Secured("ROLE_ADMIN")
+    @PermitAll
     @GetMapping("/{productId}")
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Gets a specific product with the give Id", security = @SecurityRequirement(name = "apiKey"))
