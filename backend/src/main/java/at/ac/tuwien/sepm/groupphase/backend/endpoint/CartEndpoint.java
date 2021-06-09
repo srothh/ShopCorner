92e89a5a402141d842d995745f11a665b4ea6bc9
Christian Ammann <e11713963@student.tuwien.ac.at>
2021-06-09 12:10:21 +0200

    [#71] changed the sessionId request

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/CartEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/CartEndpoint.java
@@ -7,10 +7,10 @@ import at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper.ProductMapper;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Product;
 import at.ac.tuwien.sepm.groupphase.backend.service.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.util.Pair;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -18,20 +18,26 @@ import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.security.PermitAll;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.lang.invoke.MethodHandles;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+@CrossOrigin(origins = "http://localhost", allowCredentials = "true")
 @RestController
 @RequestMapping("api/v1/carts")
 public class CartEndpoint {
@@ -47,41 +53,42 @@ public class CartEndpoint {
         this.productService = productService;
     }
 
-    @CrossOrigin(origins = "http://localhost:8080")
     @PermitAll
-    @PostMapping
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Generate session", security = @SecurityRequirement(name = "apiKey"))
+    public ResponseEntity setSessionId(HttpServletResponse response) {
+        ResponseCookie resCookie = ResponseCookie.from("sessionId",UUID.randomUUID().toString())
+            .httpOnly(true)
+            .sameSite("None")
+            .secure(true)
+            .path("/")
+            .build();
+        response.addHeader("Set-Cookie", resCookie.toString());
+        return ResponseEntity.ok("request succeeded");
+    }
+
+
+    @PermitAll
+    @PutMapping
     @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "add a new product to cart")
-    public ResponseEntity<CartDto> addProductToCart(@CookieValue(name = "sessionId", defaultValue = "default") String sessionId, @RequestBody List<ProductDto> productDtos) {
+    @Operation(summary = "add a new product to cart", security = @SecurityRequirement(name = "apiKey"))
+    public CartDto addProductToCart(@CookieValue(name = "sessionId", defaultValue = "default") String sessionId, @RequestBody List<ProductDto> productDtos, HttpServletRequest httpServletRequest) {
         LOGGER.info("POST api/v1/carts {}", productDtos);
         List<Product> cart;
-        System.out.println(sessionId);
-        if (sessionId.equals("default") || cartMap.get(sessionId) == null) {
-            sessionId = UUID.randomUUID().toString();
-            cart = productMapper.dtoToEntity(productDtos);
-            cartMap.put(sessionId, cart);
-        } else {
+        System.out.println(httpServletRequest.getCookies());
+        if (!sessionId.equals("default")) {
             cart = productMapper.dtoToEntity(productDtos);
             cartMap.put(sessionId, cart);
         }
+
         Map<Long, Integer> cartItems  = new HashMap<>();
         cartItems.put(1L, 2);
         cartItems.put(2L, 2);
         cartItems.put(3L, 2);
-        ResponseCookie.ResponseCookieBuilder responseCookieBuilder = ResponseCookie.from("sessionId", sessionId);
-        return new ResponseEntity<>(new CartDto(cartItems), this.generateHeader(responseCookieBuilder.build()), HttpStatus.CREATED);
+        return new CartDto(cartItems);
     }
 
-    /**
-     * generates response header for the create and get cart with cookie including sessionId.
-     *
-     * @return header for response
-     */
-    private HttpHeaders generateHeader(ResponseCookie responseCookie) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.add(HttpHeaders.COOKIE, responseCookie.toString());
-        return headers;
-    }
+
 
 }
--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -15,6 +15,32 @@ export class CartService {
 
   // TODO: {withCredentials: true} throws an CORS error
   addProductsToCart(products: Product[]): Observable<any> {
-    return this.httpClient.post<any>(this.cartBaseUri,  products);
+      return this.httpClient.put<any>(this.cartBaseUri, products);
   }
+
+  getSessionAuthentication(products: Product[]): Observable<any> {
+    return this.httpClient.get<any>(this.cartBaseUri);
+  }
+
+  private getCookie() {
+    const name = 'sessionId';
+    const dc = document.cookie;
+    const prefix = name + '=';
+    let begin = dc.indexOf('; ' + prefix);
+    if (begin === -1) {
+      begin = dc.indexOf(prefix);
+      if (begin !== 0) {
+        return false;
+      }
+    } else {
+      begin += 2;
+      let end = document.cookie.indexOf(';', begin);
+      if (end === -1) {
+        end = dc.length;
+      }
+      return true;
+    }
+  }
+
 }
+
