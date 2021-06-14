5e02b5b0751e9ae716b76323b63cb1f1924fda38
Hafiz Khan <hafiz_10@hotmail.com>
2021-06-14 13:00:19 +0200

    [#91] fixed frontend bugs

--- frontend/src/app/components/operator/operator-category-details/operator-category-details.component.ts
+++ frontend/src/app/components/operator/operator-category-details/operator-category-details.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Category} from '../../../dtos/category';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {CategoryService} from '../../../services/category.service';
 import {OperatorAuthService} from '../../../services/auth/operator-auth.service';
 
@@ -13,21 +13,34 @@ export class OperatorCategoryDetailsComponent implements OnInit {
   errorOccurred: boolean;
   errorMessage: string;
   selectedCategory: Category;
-  constructor(private router: Router, private categoryService: CategoryService, private authService: OperatorAuthService) {
+
+  constructor(private router: Router,
+              private categoryService: CategoryService,
+              private authService: OperatorAuthService,
+              private activatedRouter: ActivatedRoute) {
     if (router.getCurrentNavigation().extras.state !== undefined) {
       this.selectedCategory = this.router.getCurrentNavigation().extras.state[0] as Category;
     }
   }
+
   ngOnInit(): void {
   }
-  deleteCategory(){
-    this.categoryService.deleteCategory(this.selectedCategory.id).subscribe(() => {
+
+  deleteCategory() {
+    let categoryId;
+    if (this.selectedCategory === undefined) {
+      categoryId = +this.activatedRouter.snapshot.paramMap.get('id');
+    } else {
+      categoryId = this.selectedCategory.id;
+    }
+    this.categoryService.deleteCategory(categoryId).subscribe(() => {
       this.router.navigate(['operator/categories']).then();
     }, error => {
       this.errorMessage = error.error.message;
       this.errorOccurred = true;
     });
   }
+
   getPermission(): string {
     return this.authService.getUserRole();
   }
