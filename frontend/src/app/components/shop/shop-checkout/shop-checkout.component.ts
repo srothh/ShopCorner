ea84210020d41e10877865014986bd4db71c569f
Simon Roth <e11907576@student.tuwien.ac.at>
2021-06-14 20:50:22 +0200

    [#72] improved checkout ui

--- frontend/src/app/components/shop/shop-checkout/shop-checkout.component.ts
+++ frontend/src/app/components/shop/shop-checkout/shop-checkout.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Customer} from '../../../dtos/customer';
 import {MeService} from '../../../services/me.service';
+import {CartService} from '../../../services/cart.service';
+import {CartGlobals} from '../../../global/cartGlobals';
+import {Product} from '../../../dtos/product';
 
 @Component({
   selector: 'app-shop-checkout',
@@ -10,17 +13,47 @@ import {MeService} from '../../../services/me.service';
 export class ShopCheckoutComponent implements OnInit {
 
   customer: Customer;
+  products: Product[];
 
-  constructor(private meService: MeService) {
+  constructor(private meService: MeService, private cartService: CartService, private cartGlobals: CartGlobals) {
   }
 
   ngOnInit(): void {
     this.fetchCustomer();
+    this.products = this.cartGlobals.getCart();
   }
 
   getStreetAddress(): string {
     return this.customer.address.street + ' ' + this.customer.address.houseNumber + (this.customer.address.doorNumber
       ? '/' + this.customer.address.doorNumber : '');
+  }
+
+  getCartSize() {
+    return this.cartGlobals.getCartSize();
+  }
+
+  getTotalPrice() {
+    let price = 0;
+    for (const item of this.products) {
+      price += item.price;
+    }
+    return price;
+  }
+
+  getTotalPriceWithoutTaxes() {
+    let price = 0;
+    for (const item of this.products) {
+      price += item.price - ((item.price / (item.taxRate.percentage)));
+    }
+    return price;
+  }
+
+  getTotalTaxes() {
+    let price = 0;
+    for (const item of this.products) {
+      price += (item.price / (item.taxRate.percentage));
+    }
+    return price;
   }
 
   private fetchCustomer() {
@@ -30,6 +63,5 @@ export class ShopCheckoutComponent implements OnInit {
       }
     );
   }
-
 
 }
