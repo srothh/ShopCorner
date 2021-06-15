74d9b851023be14852d851700e715987e997e60c
Christian Ammann <e11713963@student.tuwien.ac.at>
2021-06-15 19:30:23 +0200

    [#95] added documentation to the PdfGeneratorService

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/PdfGeneratorService.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/PdfGeneratorService.java
@@ -6,5 +6,12 @@ import at.ac.tuwien.sepm.groupphase.backend.entity.Invoice;
  * A service class generates pdf files from saved invoices.
  */
 public interface PdfGeneratorService {
+
+    /**
+     * Creates a pdf from a given invoice.
+     *
+     * @param invoice the invoice to be create as pdf
+     * @return byte array includes pdf.
+     */
     byte[] createPdfInvoiceOperator(Invoice invoice);
 }
