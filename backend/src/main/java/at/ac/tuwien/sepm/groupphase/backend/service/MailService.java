67dc170744ca0f6c3b246ebfd96e3342a83f4c47
Michael <e11705362@tuwien.student.ac.at>
2021-06-17 19:43:26 +0200

    [#65] added JavaDoc to MailService

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/MailService.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/MailService.java
@@ -2,7 +2,15 @@ package at.ac.tuwien.sepm.groupphase.backend.service;
 
 import at.ac.tuwien.sepm.groupphase.backend.entity.Order;
 
+/**
+ * Service that handles E-Mails
+ */
 public interface MailService {
 
+    /**
+     * Sends an order confirmation email to customer
+     *
+     * @param order for which confirmation should be sent
+     */
     void sendMail(Order order);
 }
