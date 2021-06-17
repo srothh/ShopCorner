7b9f599492b72203a7a2a50abaf8122020b43be8
Michael <e11705362@tuwien.student.ac.at>
2021-06-17 19:44:46 +0200

    [#65] added throws to JavaDoc

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/MailService.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/MailService.java
@@ -2,6 +2,8 @@ package at.ac.tuwien.sepm.groupphase.backend.service;
 
 import at.ac.tuwien.sepm.groupphase.backend.entity.Order;
 
+import at.ac.tuwien.sepm.groupphase.backend.exception.ServiceException;
+
 /**
  * Service that handles E-Mails
  */
@@ -11,6 +13,7 @@ public interface MailService {
      * Sends an order confirmation email to customer
      *
      * @param order for which confirmation should be sent
+     * @throws ServiceException if something goes wrong while creating E-Mail
      */
     void sendMail(Order order);
 }
