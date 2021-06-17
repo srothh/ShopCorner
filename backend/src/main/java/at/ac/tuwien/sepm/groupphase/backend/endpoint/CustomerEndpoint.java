8a5427d8e920543cde1cd26a55ec3b73dfe3adab
Christian Ammann <e11713963@student.tuwien.ac.at>
2021-06-17 19:34:23 +0200

    [#77] removed a printLine from the CustomerEndpoint

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/CustomerEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/CustomerEndpoint.java
@@ -105,7 +105,6 @@ public class CustomerEndpoint {
     @Operation(summary = "Retrieve customer from order", security = @SecurityRequirement(name = "apiKey"))
     public CustomerDto getCustomerById(@PathVariable  Long id) {
         LOGGER.info("GET api/v1/invoices?invoice={}", id);
-        System.out.println(id);
         return this.customerMapper.customerToCustomerDto(this.customerService.findCustomerById(id));
     }
 
