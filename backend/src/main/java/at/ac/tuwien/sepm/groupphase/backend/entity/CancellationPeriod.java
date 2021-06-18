331b75eef0058c7022c10c3d8f6fa4375b54c3a4
Simon Roth <e11907576@student.tuwien.ac.at>
2021-06-18 20:14:54 +0200

    [#79] added testing for cancellationperiod.

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/CancellationPeriod.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/CancellationPeriod.java
@@ -1,12 +1,12 @@
 package at.ac.tuwien.sepm.groupphase.backend.entity;
 
+import java.util.Objects;
+
 public class CancellationPeriod {
     private int days;
 
     public CancellationPeriod() {
     }
-
-    ;
 
     public CancellationPeriod(int days) {
         this.days = days;
@@ -18,5 +18,17 @@ public class CancellationPeriod {
 
     public void setDays(int days) {
         this.days = days;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CancellationPeriod cancellationPeriod = (CancellationPeriod) o;
+        return Objects.equals(this.days, cancellationPeriod.days);
     }
 }
