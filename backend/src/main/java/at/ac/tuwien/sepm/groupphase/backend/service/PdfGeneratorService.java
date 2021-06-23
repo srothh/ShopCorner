98a8d5faf4a12ca1a771c76a81b5c898a5b8f652
Christian Ammann <e11713963@student.tuwien.ac.at>
2021-06-23 17:20:31 +0200

    [#100] added javadoc to the PdfGeneratorService

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/PdfGeneratorService.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/PdfGeneratorService.java
@@ -9,7 +9,7 @@ import at.ac.tuwien.sepm.groupphase.backend.entity.Order;
 public interface PdfGeneratorService {
 
     /**
-     * Creates a pdf for operators from a given invoice.
+     * Creates a invoice pdf for operators from a given invoice.
      *
      * @param invoice the invoice to be create as pdf
      * @return byte array includes pdf.
@@ -17,7 +17,15 @@ public interface PdfGeneratorService {
     byte[] createPdfInvoiceOperator(Invoice invoice);
 
     /**
-     * Creates a pdf of a customer for operator from a given invoice.
+     * Creates a canceled invoice pdf for operators from a given invoice.
+     *
+     * @param invoice to generate a pdf of it
+     * @return byte array canceled invoice pdf.
+     */
+    byte[] createPdfCanceledInvoiceOperator(Invoice invoice);
+
+    /**
+     * Creates a canceled invoice pdf of a customer for operator from a given invoice.
      *
      * @param invoice the invoice to be create as pdf
      * @return byte array includes pdf.
@@ -25,15 +33,20 @@ public interface PdfGeneratorService {
     byte[] createPdfInvoiceCustomerFromInvoice(Invoice invoice);
 
     /**
-     * Creates a pdf for customers from a given order.
+     * Creates a canceled invoice pdf for customers from a given order.
      *
      * @param order to generate a pdf of it
      * @return byte array includes pdf.
      */
     byte[] createPdfInvoiceCustomer(Order order);
 
-
+    /**
+     * Creates a canceled invoice pdf for customers from a given order.
+     *
+     * @param order to generate a pdf of it
+     * @return byte array canceled invoice pdf.
+     */
     byte[] createPdfCanceledInvoiceCustomer(Order order);
 
-    byte[] createPdfCanceledInvoiceOperator(Invoice invoice);
+
 }
