b2445a82abd6c2a500847c23aa2fe34aeda43f35
Simon Roth <e11907576@student.tuwien.ac.at>
2021-06-25 20:36:31 +0200

    [#122] Added order service tests

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/unittests/OrderServiceTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/unittests/OrderServiceTest.java
@@ -3,15 +3,14 @@ package at.ac.tuwien.sepm.groupphase.backend.unittests;
 import at.ac.tuwien.sepm.groupphase.backend.basetest.TestData;
 import at.ac.tuwien.sepm.groupphase.backend.entity.CancellationPeriod;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Order;
-import at.ac.tuwien.sepm.groupphase.backend.repository.OrderRepository;
-import at.ac.tuwien.sepm.groupphase.backend.service.InvoiceService;
-import at.ac.tuwien.sepm.groupphase.backend.service.MailService;
-import at.ac.tuwien.sepm.groupphase.backend.service.OrderService;
-import org.junit.Test;
+import at.ac.tuwien.sepm.groupphase.backend.exception.ServiceException;
+import at.ac.tuwien.sepm.groupphase.backend.service.impl.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
@@ -21,8 +20,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.io.IOException;
 import java.util.UUID;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
@@ -31,20 +30,32 @@ import static org.mockito.Mockito.when;
 public class OrderServiceTest implements TestData {
 
     @Mock
-    private OrderService orderService;
-    @Mock
     Order order;
+    @Spy
+    @InjectMocks
+    private OrderServiceImpl orderService;
     @Mock
     private CancellationPeriod cancellationPeriod;
-    UUID sessionID = UUID.randomUUID();
+    private UUID sessionID = UUID.randomUUID();
 
     @Test
-    public void placeNewOrderWithInvoice_thenReturnOrder(){
-        when(orderService.placeNewOrder(order, sessionID.toString())).thenReturn(order);
+    public void placeNewOrderWithInvoice_thenReturnOrder() {
+        doReturn(order).when(orderService).placeNewOrder(order, sessionID.toString());
     }
 
     @Test
     public void setCancellationPeriod_thenReturnCancellationPeriod() throws IOException {
-        when(orderService.setCancellationPeriod(cancellationPeriod)).thenReturn(cancellationPeriod);
+        doReturn(cancellationPeriod).when(orderService).setCancellationPeriod(cancellationPeriod);
     }
+
+    @Test
+    public void whenPlaceNewOrderWithInvalidSessionId_thenThrowServiceException() {
+        doThrow(ServiceException.class).when(orderService).placeNewOrder(order, "default");
+    }
+
+    @Test
+    public void whenGetAllOrdersWithNoOrdersPersisted_thenThrowNullPointerException() {
+        doThrow(NullPointerException.class).when(orderService).getAllOrders(0,10);
+    }
+
 }
